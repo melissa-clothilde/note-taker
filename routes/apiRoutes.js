4ed982b64b2b246895e6b2629d674ffbc76160c0
Aurora Sky <62398803+melissa-clothilde@users.noreply.github.com>
2020-06-27 15:00:04 -0700

    updates fronent, and completes api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,43 +3,57 @@ const router = require("express").Router();
 
 const fs = require("fs");
 const path = require("path");
-const OUTPUT_DIR = path.resolve(__dirname, "db");
+const { Router } = require("express");
+const OUTPUT_DIR = path.resolve(__dirname, "../db");
 const outputPath = path.join(OUTPUT_DIR, "db.json");
 
 const notes = [];
+let id = 0;
 
 function readData() {
-  fs.readFile(outputPath, function (error, data) {
-
-    if (error) {
-      return console.log(error);
-    }
-
-    else {
-      notes = JSON.parse(data);
-    }
-  });
+  const data = fs.readFileSync(outputPath);
+  return JSON.parse(data);
 };
 
-function writeData() {
-  fs.writeFile(outputPath, JSON.stringify(notes), function (error, data) {
+function writeData(req, res) {
+  const newNote = req.body;
+  newNote.id = id;
+  id += 1;
+  notes.push(newNote);
+  fs.writeFileSync(outputPath, JSON.stringify(notes), function (error) {
 
     if (error) {
-      return console.log(error);
-    }
-
-    else {
-      console.log("File written!")
+      throw error;
     }
   });
+  console.log("File written!")
+  return notes;
 };
 
+router.get("/notes", function (req, res) {
+  res.json(readData());
+});
 
-  app.get("/api/notes", function (req, res) {
-    return res.json(notes);
+router.post("/notes", function (req, res) {
+  res.json(writeData(req, res));
+});
+
+
+router.delete("/notes/:id", function (req, res) {
+  const notes = readData();
+  const newNotes = notes.filter(note => note.id!==Number(req.params.id));
+  console.log(newNotes, req.params.id);
+  fs.writeFileSync(outputPath, JSON.stringify(newNotes), function (error) {
+
+    if (error) {
+      throw error;
+    }
   });
+  res.json(newNotes);
+});
 
 
 
-  //Create POST (call writefile) and DELETE API routes (counter increeasing, or uuid package to generate random ids, when deleting note, fetch id to delete it)
+//Create DELETE API route (counter increeasing, or uuid package to generate random ids, when deleting note, fetch id to delete it)
 
+module.exports = router;
